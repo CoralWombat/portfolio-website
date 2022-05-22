2238f3726d0a429c59d09627140a5490d68d6a78
Kristóf Göncző <kristofgonczo@gmail.com>
2022-05-22 20:33:58 +0200

    modifying margins

--- src/components/widgets/ProjectsCard.js
+++ src/components/widgets/ProjectsCard.js
@@ -3,16 +3,16 @@ import React from 'react';
 class ProjectsCard extends React.Component {
     render() {
         return (
-            <div className='bg-custom-light text-custom-dark rounded-2xl shadow-2xl'>
+            <div className='bg-custom-light text-custom-dark rounded-3xl shadow-2xl'>
 
-                <div className='bg-custom-color text-custom-light rounded-t-lg w-full py-3 text-center lg:rounded-t-xl'>
+                <div className='bg-custom-color text-custom-light rounded-t-3xl w-full py-8 text-center lg:rounded-t-xl'>
                     <h4>{this.props.lifeSpan}</h4>
                     <h3>{this.props.title}</h3>
                     <h5>as {this.props.role}</h5>
                     <h3 className='flex justify-center'>{this.props.icon}</h3>
                 </div>
 
-                <div className='m-5'>
+                <div className='m-12'>
                     <p>
                         {this.props.text}
                     </p>
