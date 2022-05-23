65b13b52c83604ba41df17a30b63a651e7f8418b
Kristóf Göncző <kristofgonczo@gmail.com>
2022-05-23 16:41:35 +0200

    Fixed footer horizontal overflow

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -5,7 +5,7 @@ import { SiTailwindcss } from 'react-icons/si';
 
 class Footer extends React.Component {
     render() {
-        return <div className='w-screen bg-custom-color p-16' >
+        return <div className='bg-custom-color p-16' >
             <footer>Handcrafted by me &nbsp;<BiCopyright />&nbsp; with React <FaReact />&nbsp; and Tailwind CSS <SiTailwindcss />.</footer>
         </div>
     }
